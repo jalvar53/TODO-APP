be3adf42116c58d15e3dd7cb90d4e9905f9fe068
Jose Luis <jalvarezh@psl.com.co>
2017-11-23 09:47:38 -0500

    More changes to configuration files to enable testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,18 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', '@angular/cli'],
     files: [
     ],
     exclude: [
     ],
     preprocessors: {
     },
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('@angular/cli/plugins/karma')
+    ],
     reporters: ['progress'],
     port: 9876,
     colors: true,
