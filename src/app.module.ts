2949dd39a2380e37a231c6705b6edf3950d8955d
Jose Luis <jalvarezh@psl.com.co>
2017-11-23 09:10:02 -0500

    Adding missing config files and fixing lint errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app/app.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
