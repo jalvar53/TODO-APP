13a1e4aefc0edf13703a24180322a45f18326153
Jose Luis Alvarez Herrera <jalvar53@eafit.edu.co>
2017-11-23 13:57:28 -0500

    App start (#2)
    * Update README.md
    
    * Boilerplate to start angular app
    
    * Fixed first commit
    
    * EOF fixed
    
    * Other minor fixes about the configuration files
    
    * Initial application setup, bootstrapping the app and empty root component
    
    * fix npm scripts
    
    * Adding missing config files and fixing lint errors
    
    * More changes to configuration files to enable testing
    
    * Some other config fixes and firsts unit tests
    
    * Following conventions and code cleaning for the tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,22 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine'],
-    files: [
-    ],
-    exclude: [
-    ],
+    frameworks: ['jasmine', '@angular/cli'],
     preprocessors: {
     },
+    files: [
+      {pattern: 'node_modules/zone.js/dist/zone.js', included: true, watched: true}
+    ],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('@angular/cli/plugins/karma')
+    ],
     reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
+    autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
     concurrency: Infinity
