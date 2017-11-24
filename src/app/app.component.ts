a90df1761d007317dce05bf0e0b0367bb201ed2d
Jose Luis <jalvarezh@psl.com.co>
2017-11-24 13:47:56 -0500

    More testing, adding delete task feature and a Task class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
+import { Task } from './model/task.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
 
-  tasks: Array<String> = [];
+  tasks: Array<Task> = [];
 
-  addNewtask (newTask: String) {
-    this.tasks.push(newTask);
+  addNewtask (newTaskName: String) {
+    const addedTask = new Task(newTaskName, 'Active');
+    if (!this.inputIsEmpty(newTaskName)) {
+      this.tasks.push(addedTask);
+    }
+  }
+
+  removeTask(taskToRemove: String) {
+    const indexOfTask = this.tasks.findIndex(x => x.name === taskToRemove);
+    this.tasks.splice(indexOfTask, 1);
+  }
+
+  inputIsEmpty(input: String) {
+    if (input === '') {
+      return true;
+    }
+    return false;
   }
 
 }
